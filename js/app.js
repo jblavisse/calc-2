6ec54193435c49955ef0706cd9d93870b3c4e88a
jblavisse <contact@jlavisse.com>
2020-05-29 16:40:21 +0200

    Create HTML&CSS structure for one calculator

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-import Calculator from './calculator.js';
+// import Calculator from './calculator.js';
 
-let calc = new Calculator();
-calc.init();
+// let calc = new Calculator();
+// calc.init();
 
-let calc2 = new Calculator();
-calc2.init();
+// let calc2 = new Calculator();
+// calc2.init();
+
+
