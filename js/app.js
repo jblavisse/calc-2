7ed87f9fe54b547c88f1be4189b543a0652f64d8
jblavisse <contact@jlavisse.com>
2020-05-29 17:29:05 +0200

    Move to Calc Constructor

--- js/app.js
+++ js/app.js
@@ -1,63 +1,14 @@
-// import Calculator from './calculator.js';
+import Calculator from './calculator.js';
 
-// let calc = new Calculator();
-// calc.init();
+let calc = new Calculator();
+calc.init();
 
-// let calc2 = new Calculator();
-// calc2.init();
+let calc2 = new Calculator();
+calc2.init();
 
 
-function createHTMLElement(tag,classes,parentElement,text) {
-    let el = document.createElement(tag);
-    if(classes) {
-        el.classList.add(classes);
-    }
-    
-    if(text) {
-        el.textContent = text;
-    }
 
-    parentElement.append(el);
-    return el; 
-}
 
-let calculator = createHTMLElement("div","calcul",document.body);
-let screen = createHTMLElement("header","calcul__head",calculator);
-let ul = createHTMLElement("ul","",calculator);
-let li =  createHTMLElement("li","",ul);
-
-let lisArr = [
-    {
-        class: "calcul__operator",
-        id: "+",
-        text: "+",
-        number: 1
-    },
-    {
-        class: "calcul__operator",
-        id: "-",
-        text: "-",
-        number: 1
-    },
-    {
-        class: "calcul__operator",
-        id: "*",
-        text: "x",
-        number: 1
-    },
-    {
-        class: "calcul__operator",
-        id: "/",
-        text: "/",
-        number: 1
-    },
-    {
-        class: "calcul__element",
-        id: 7,
-        text: "7",
-        number: 2
-    }
-]
 
 // Idea for addEventListener calculator 
 // let class = event.target.className;
@@ -72,9 +23,3 @@ let lisArr = [
 //         eval(screen.textContent)
 // }
 
-
-let button;
-
-for(let i=0;i<lisArr.length;i++) {
-    button = createHTMLElement("button",lisArr[i].class,li,lisArr[i].text);
-}
--- js/calculator.js
+++ js/calculator.js
@@ -2,15 +2,66 @@ export default function Calculator() {
     this.name = "calculatrice";
     this.element = null;
 
-    this.init = function() {
-        this.element = document.createElement("div");
-        this.element.classList.add("calculator");
-        document.body.prepend(this.element);
 
+    this.createHTMLElement = function(tag,classes,parentElement,text) {
+        let el = document.createElement(tag);
+        if(classes) {
+            el.classList.add(classes);
+        }
         
-        let button = document.createElement("button");
-        button.textContent = "1";
-        this.element.append(button);
+        if(text) {
+            el.textContent = text;
+        }
+
+        parentElement.append(el);
+        return el; 
+    }
+
+    this.init = function() {
+        let calculator = this.createHTMLElement("div","calcul",document.body);
+        let screen = this.createHTMLElement("header","calcul__head",calculator);
+        let ul = this.createHTMLElement("ul","",calculator);
+        let li =  this.createHTMLElement("li","",ul);
+        
+        let lisArr = [
+            {
+                class: "calcul__operator",
+                id: "+",
+                text: "+",
+                number: 1
+            },
+            {
+                class: "calcul__operator",
+                id: "-",
+                text: "-",
+                number: 1
+            },
+            {
+                class: "calcul__operator",
+                id: "*",
+                text: "x",
+                number: 1
+            },
+            {
+                class: "calcul__operator",
+                id: "/",
+                text: "/",
+                number: 1
+            },
+            {
+                class: "calcul__element",
+                id: 7,
+                text: "7",
+                number: 2
+            }
+        ]
+        
+        
+        let button;
+        
+        for(let i=0;i<lisArr.length;i++) {
+            button = this.createHTMLElement("button",lisArr[i].class,li,lisArr[i].text);
+        }
     }
 
 }
